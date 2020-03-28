8885a96d969653e0cce6ab38cd86b8a3f2ba1034
alicimustafa <alicimustafadev@yahoo.com>
2020-03-28 17:33:53 -0600

    added a popup login modal

--- PandemicSupplyAngular/src/app/navbar/navbar.component.ts
+++ PandemicSupplyAngular/src/app/navbar/navbar.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  buttonVal : String;
+
   constructor() { }
 
   ngOnInit() {
+    this.buttonVal = 'Log-in';
   }
 
+  userInfo(){
+    this.buttonVal = 'Log-out';
+    document.getElementById('close-modal').click();
+  }
+
+  modelButtonResponse(){
+    if(this.buttonVal == 'Log-in'){
+      document.getElementById('open-modal').click();
+    } else {
+      this.buttonVal = 'Log-in';
+    }
+  }
 }
