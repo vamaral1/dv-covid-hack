afc4e98ec57472b71daf720d3ca48e0a5badcdd5
Samantha Janik <midgetmode19@gmail.com>
2020-03-29 02:18:41 -0600

    Added bootstrap and a mock table for the supplies list page

--- PandemicSupplyAngular/src/app/suply-list/suply-list.component.ts
+++ PandemicSupplyAngular/src/app/suply-list/suply-list.component.ts
@@ -1,5 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Item {
+  name: string;
+  type: string;
+  size: string;
+}
+
+const ITEMS: Item[] = [
+  {
+    name: 'Ventilator',
+    type: 'Non-invasive',
+    size: 'Pediatric'
+  },
+  {
+    name: 'Mask',
+    type: 'Respirator',
+    size: 'Large'
+  },
+  {
+    name: 'Mask',
+    type: 'Surgical',
+    size: 'Small'
+  },
+  {
+    name: 'Gloves',
+    type: 'Nitrile',
+    size: 'Medium'
+  }
+];
+
 @Component({
   selector: 'app-suply-list',
   templateUrl: './suply-list.component.html',
@@ -8,6 +37,8 @@ import { Component, OnInit } from '@angular/core';
 export class SuplyListComponent implements OnInit {
 
   constructor() { }
+
+  items = ITEMS;
 
   ngOnInit() {
   }
